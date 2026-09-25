ef52f7174e97803da666e50e92ef256cc27aa81a
LAYON <rodriguezgarciadavid6@gmail.com>
2026-09-25 14:13:31 -0600

    revision 2

--- Negative Client/Negative Client/obj/Debug/net10.0-windows/Negative Client.AssemblyInfo.cs
+++ Negative Client/Negative Client/obj/Debug/net10.0-windows/Negative Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459b42fd61da64f6bc4d2d2c236c653628687262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770750233ef2e77be016eb8bb97fc63b356a6d31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
