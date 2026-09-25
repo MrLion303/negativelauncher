8fefb5dad73b22d2b8b16c51b6fdf20b28fefbd1
LAYON <rodriguezgarciadavid6@gmail.com>
2026-09-25 17:01:22 -0600

    dipto

--- Negative Client/Negative Client/obj/Debug/net10.0-windows/Negative Client.AssemblyInfo.cs
+++ Negative Client/Negative Client/obj/Debug/net10.0-windows/Negative Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65fa0b554040463241b449a64c9aad66c71de73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f068b26913d5eb3a14838b0630e4543576219db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negative Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
